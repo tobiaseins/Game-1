6d45423f2554503625f94cc5b91399c3924467fd
NKayser <nico.a.kayser@gmail.com>
2018-01-29 11:08:46 +0100

    Ich habe den Code etwas verschönert

--- src/Fenster.java
+++ src/Fenster.java
@@ -7,34 +7,24 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Fenster extends JFrame implements KeyListener{
-	
+	// Variablen
 	private Point Groesse = new Point(887,340);
 	
 	//Konstruktor
-	public Fenster()
-	{
-		//Titel setzten
-		setTitle("PacMan");
-		
-		//Füge die Tastenerkennung hinzu
-		addKeyListener(this);
-		
-		//Größe festlegen
-		setSize(887,340);
-		
-		//sichtbar machen
-    	setVisible(true);
+	public Fenster() {
+		setTitle("PacMan");				// Titel setzten
+		addKeyListener(this);			// Füge die Tastenerkennung hinzu
+		setSize(887,340);				// Größe festlegen
+    	setVisible(true);				// sichtbar machen
 	}
 	
 	//auf Tastendrücke reagieren
 	public void keyPressed(KeyEvent e) {
-		
-		//Tastencode auslesen
-		int keyCode = e.getKeyCode();
+		int keyCode = e.getKeyCode();	// Tastencode auslesen
 		
 		//Tastencode vergleichen und die Bewegungsrichtung festlegen
 		//Bewegung: 1 = rechts; 2 = links; 3 = oben; 4 = unten
-	    switch( keyCode ) {
+	    switch(keyCode) {
 	    	case KeyEvent.VK_RIGHT: //Vergleich 
 	    		break; //Fertig - nicht mehr weitermachen
 	        case KeyEvent.VK_LEFT: //Vergleich
@@ -43,17 +33,10 @@ public class Fenster extends JFrame implements KeyListener{
 	        	break; //Fertig - nicht mehr weitermachen
 	        case KeyEvent.VK_DOWN: 
 	        	break; //Ferig - nicht mehr weitermachen
-	    }
-		        
+	    } 
 	}
 
 	//Unnötig - muss aber auftauchen - sagt der KeyListener
-	public void keyTyped(KeyEvent e)
-	{
-	}
-			 
-	//Unnötig - muss aber auftauchen - sagt der KeyListener
-	public void keyReleased(KeyEvent e)
-	{
-	}
+	public void keyTyped(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
 }
