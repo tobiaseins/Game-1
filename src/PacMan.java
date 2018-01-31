20a6acd52642933966f76e6efcb9a295d9e6b3cb
MButenob <moritz@butenob.net>
2018-01-31 12:39:44 +0100

    audio

--- src/PacMan.java
+++ src/PacMan.java
@@ -1,6 +1,5 @@
-
 public class PacMan extends Figuren{
-	
+	// Variablen nur für Pacman
 	private int Winkel_min;
 	private int Winkel_max;
 	private int leben;
@@ -9,6 +8,11 @@ public class PacMan extends Figuren{
 	private Audio audio_sterben;
 	private Audio audio_intro;
 	
+	// Getter- und Settermethoden
+	public int get_Winkel_min () {return Winkel_min;}
+	public int get_Winkel_max () {return Winkel_max;}
+	public int get_leben () {return leben;}
+	public boolean get_mund_offen () {return mund_offen;}
 	
 	public PacMan(){
 		this.Winkel_min = 270;
@@ -19,39 +23,8 @@ public class PacMan extends Figuren{
 		this.audio_sterben = new Audio("pacman_sterben");
 		this.audio_intro = new Audio("pacman_intro");
 	}
-	//Getter- und Settermethoden
-	public int get_Winkel_min () {
-		return Winkel_min;
-	}
-	
-	public void set_Winkel_min (int newWinkel_min){
-		Winkel_min = newWinkel_min;
-	}
-	
-	public int get_Winkel_max () {
-		return Winkel_max;
-	}
-	
-	public void set_Winkel_max (int newWinkel_max) {
-		Winkel_max = newWinkel_max;
-	}
-	
-	public int get_leben () {
-		return leben;
-	}
-	
-	public void set_leben (int newLeben) {
-		leben = newLeben;
-	}	
-	
-	public boolean get_mund_offen () {
-		return mund_offen;
-	}
-	
-	public void set_mund_offen (boolean newMund_offen) {
-		mund_offen = newMund_offen;
-	}
-	
-	//Getter- und Settermethoden ende
-	
+	public void set_Winkel_min (int newWinkel_min) {Winkel_min = newWinkel_min;}
+	public void set_Winkel_max (int newWinkel_max) {Winkel_max = newWinkel_max;}
+	public void set_leben (int newLeben) {leben = newLeben;}
+	public void set_mund_offen (boolean newMund_offen) {mund_offen = newMund_offen;}
 }
