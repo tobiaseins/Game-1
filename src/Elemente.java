6c137da7f9c415b4896a83e0cacc1fbb51a45124
PascalSoppa <pascalsoppa.ps@gmail.com>
2018-01-31 13:14:17 +0100

    Füge Elemente dem Fenster hinzu

---
+++ src/Elemente.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+
+public class Elemente extends Figuren {
+	
+	public Elemente() {
+		System.out.println("B");
+	}
+	
+	
+		// zeichnen
+		public void paintComponent(Graphics g)
+		{
+		    g.setColor(Color.BLACK); //Schwarzer Hintergrund
+		    g.fillRect(0, 0, getWidth(), getHeight());
+			
+		    repaint();	//alles neu zeichnen
+		    System.out.println("A");
+		}
+		
+}
