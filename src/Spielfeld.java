b60820abbe79e68d20c39a440dcb15f1fdeedacc
PascalSoppa <pascalsoppa.ps@gmail.com>
2018-02-07 13:09:45 +0100

    Hinzufügen der Geisterwände

--- src/Spielfeld.java
+++ src/Spielfeld.java
@@ -2,6 +2,7 @@ public class Spielfeld {
 	public int[] hintergrund_Farbe = {0,0,0};
 	public int[] farbe_Punkte = {255,255,255};
 	public int[] farbe_Waende = {24,30,182};
+	public int[] farbe_Geister_Waende = {182,30,24};
 	public int raster_Groesse = 30;
 	
 	// 0 = leeres Feld; 1 = Wand; 2 = Punkt; 3 = Hurensohnfeld 
@@ -34,4 +35,5 @@ public class Spielfeld {
 	public int[] get_Hintergrundfarbe() {return hintergrund_Farbe;}
 	public int[] get_farbe_Punkte() {return farbe_Punkte;}
 	public int[] get_farbe_Waende() {return farbe_Waende;}
+	public int[] get_farbe_Geister_Waende() {return farbe_Geister_Waende;}
 }
