0758a4965e661bf055353928d0d3fdca6c1c318b
PascalSoppa <pascalsoppa.ps@gmail.com>
2018-02-12 11:58:00 +0100

    12.02.2018 Abschluss

--- src/Geist.java
+++ src/Geist.java
@@ -12,6 +12,10 @@ public class Geist extends Figuren {
 	BufferedImage imgup = null;
 	BufferedImage imgdown = null;
 	public int geist_art; // 1 = normal; 2 = aggressiv; 3 = ...
+	
+	public void richtungs_update(int x) {
+		this.set_soll_richtung(x);
+	}
 
 	
 	public Geist(Point p, int geist_art) {											// noch keine Verwendung
