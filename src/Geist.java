9bb1c7c8c9ca19e4b88ee3d45249512869d2ba29
PascalSoppa <pascalsoppa.ps@gmail.com>
2018-02-14 12:47:27 +0100

    Ein Geist der Farbe Rot einfügen und mit Richtungsupdate und wand vor Figur steuern.

--- src/Geist.java
+++ src/Geist.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,7 +8,7 @@ import java.util.Random;
 import javax.imageio.ImageIO;
 
 public class Geist extends Figuren {
-	BufferedImage imgright = null;
+	BufferedImage img = null;
 	BufferedImage imgleft = null;
 	BufferedImage imgup = null;
 	BufferedImage imgdown = null;
@@ -18,7 +19,9 @@ public class Geist extends Figuren {
 	}
 
 	
-	public Geist(Point p, int geist_art) {											// noch keine Verwendung
+	public Geist(Point p, int geist_art, Color Farbe) {											// noch keine Verwendung
+		set_radius(30);
+		set_farbe(Farbe);
 		set_position(p.x, p.y);
 		set_geist_art(geist_art);
 		set_bewegungsrichtung(1);
@@ -32,28 +35,28 @@ public class Geist extends Figuren {
 	public int get_geist_art() {return this.geist_art;}
 
 	
-	public void animation() {								// geister Blickrichtung	
+	public BufferedImage animation() {								// geister Blickrichtung	
 		if(this.get_bewegungsrichtung() == 1)
 			try{
-				imgup = ImageIO.read(new File("oben.png"));
+				img = ImageIO.read(new File("./bilder/oben.png"));
 			} catch (IOException e) {	
 			}
 		if(this.get_bewegungsrichtung() == 2)
 			try{
-				imgright = ImageIO.read(new File("rechts.png"));
+				img = ImageIO.read(new File("./bilder/rechts.png"));
 			} catch (IOException e) {
 			}
 		if(this.get_bewegungsrichtung() == 3)
 			try{
-			imgdown = ImageIO.read(new File("unten.png"));
+			img = ImageIO.read(new File("./bilder/unten.png"));
 			} catch (IOException e) {
 			}
 		if(this.get_bewegungsrichtung() == 4)
 			try{
-			imgleft = ImageIO.read(new File("links.png"));
+			img = ImageIO.read(new File("./bilder/links.png"));
 			} catch (IOException e) {
 			}
-		else System.exit(0);
+		return img;
 		
 		
 	
@@ -63,15 +66,15 @@ public class Geist extends Figuren {
 		Random ran = new Random();
 		Point Unterschied = new Point(PacPosition.x - this.get_position().x, PacPosition.y - this.get_position().y);
 		int Quadrant = 0; 										// 1: unten rechts, 2: oben rechts, 3: oben links, 4: unten links
-		if(Unterschied.x >= 0 && Unterschied.y > 0) Quadrant = 1;
-		if(Unterschied.x > 0 && Unterschied.y < 0) Quadrant = 2;
-		if(Unterschied.x < 0 && Unterschied.y <= 0) Quadrant = 3;
-		if(Unterschied.x < 0 && Unterschied.y > 0) Quadrant = 4;
+		if(Unterschied.x >= 0 && Unterschied.y > 0) Quadrant = 3;
+		if(Unterschied.x > 0 && Unterschied.y < 0) Quadrant = 4;
+		if(Unterschied.x < 0 && Unterschied.y <= 0) Quadrant = 1;
+		if(Unterschied.x < 0 && Unterschied.y > 0) Quadrant = 2;
 		
-		if(Quadrant == 1) this.set_bewegungsrichtung(ran.nextInt(2) * 3 + 1);
-		if(Quadrant == 2) this.set_bewegungsrichtung(ran.nextInt(2) * 2 + 1);
-		if(Quadrant == 3) this.set_bewegungsrichtung(ran.nextInt(2) + 2);
-		if(Quadrant == 4) this.set_bewegungsrichtung(ran.nextInt(2) * 2 + 2);
+		if(Quadrant == 1) this.set_soll_richtung(ran.nextInt(2) * 3 + 1);
+		if(Quadrant == 2) this.set_soll_richtung(ran.nextInt(2) * 2 + 1);
+		if(Quadrant == 3) this.set_soll_richtung(ran.nextInt(2) + 2);
+		if(Quadrant == 4) this.set_soll_richtung(ran.nextInt(2) * 2 + 2);
 	
 	}
 	
